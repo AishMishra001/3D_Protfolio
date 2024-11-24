06ea34adf8f75288b634ee931e136d9354fa5643
AishMishra001 <aishlunatic001@gmail.com>
2024-11-24 22:32:17 +0530

    contact section fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,11 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { IoMdMail } from "react-icons/io";
+
 
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
@@ -79,60 +84,43 @@ const Contact = () => {
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
-        >
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
-            <input
-              type='text'
-              name='name'
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your good name?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium bg-[#230438]'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
-            <input
-              type='email'
-              name='email'
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your web address?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium bg-[#230438]'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
-            <textarea
-              rows={7}
-              name='message'
-              value={form.message}
-              onChange={handleChange}
-              placeholder='What you want to say?'
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium bg-[#230438]'
-            />
-          </label>
+        <div className="flex gap-10 sm:gap-15 md:gap-25 lg:gap-40 ">
 
-          <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary bg-[#230438]'
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
+          {/* github  */}
+
+          <div className="flex mt-10">
+             <a href="https://github.com/AishMishra001/">
+             <FaGithub className="w-8 h-8 sm:w-10 sm:h-10"/>
+             </a>
+          </div>
+
+          {/* linkedin  */}
+
+          <div className="flex mt-10">
+             <a href="https://www.linkedin.com/in/aish-mishra-b24a52237/">
+             <FaLinkedin className="w-8 h-8 sm:w-10 sm:h-10"/>
+             </a>
+          </div>
+
+        
+          {/* Twitter  */}
+          <div className="flex mt-10">
+             <a href="https://x.com/AishMishra17">
+             <FaSquareXTwitter className="w-8 h-8 sm:w-10 sm:h-10"/>
+             </a>
+          </div>
+         
+         {/* gmail  */}
+          <div className="flex mt-10">
+             <a href="aish.work008@gmail.com">
+             <IoMdMail className="w-8 h-8 sm:w-10 sm:h-10"/>
+             </a>
+          </div>
+          
+        </div>
       </motion.div>
 
-      <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
-      >
-        <EarthCanvas />
-      </motion.div>
+    
     </div>
   );
 };
